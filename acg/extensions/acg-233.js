8f2903bc83dfc3c97d4ad86b331b84b10de349a1
Pisces000221 <1786762946@qq.com>
2015-02-13 18:46:37 +0800

    233: Better code and a lively example

--- acg/extensions/acg-233.js
+++ acg/extensions/acg-233.js
@@ -4,14 +4,36 @@ acg.ext = acg.ext || {};
 acg.ext._233_fontname = 'Arial';
 acg.ext._233_fontsize = 26;
 acg.ext._233_lineh = 32;
-acg.ext._233s = {};
+acg.ext._233s = [];
 
 acg.ext._233_add = function (time, text, colour, type) {
     js233.addComment(time, text, colour, type);
 };
 
 acg.ext._233_commit = function () {
-    var ops = js233.getAll();
+    js233.setDurRange(5, 10, 2, 3.5);
+    var ops = js233.getOperations();
+    ops.forEach(function (op) {
+        switch (op.type) {
+        case js233.CMT_SLIDING:
+            acg.scene.scheduleOnce(function () {
+                acg.ext._233_addSliding(op.line, op.text, op.duration, op.colour);
+            }, op.time);
+            break;
+        case js233.CMT_TOP_STICKY:
+            acg.scene.scheduleOnce(function () {
+                acg.ext._233_addSticky(1, op.line, op.text, op.duration, op.colour);
+            }, op.time);
+            break;
+        case js233.CMT_BOTTOM_STICKY:
+            acg.scene.scheduleOnce(function () {
+                acg.ext._233_addSticky(2, op.line, op.text, op.duration, op.colour);
+            }, op.time);
+            break;
+        default:
+            break;
+        }
+    });
 };
 
 acg.ext._233_addSliding = function (line, text, duration, colour) {
@@ -26,6 +48,7 @@ acg.ext._233_addSliding = function (line, text, duration, colour) {
         cc.moveBy(duration, cc.p(-(1 + lbl.getContentSize().width / acg.width), 0)),
         cc.removeSelf()));
     acg.scene.addChild(lbl);
+    acg.ext._233s.push(lbl);
 };
 
 acg.ext._233_addSticky = function (type, line, text, duration, colour) {
@@ -47,4 +70,5 @@ acg.ext._233_addSticky = function (type, line, text, duration, colour) {
     lbl.setColor(colour);
     lbl.runAction(cc.sequence(cc.delayTime(duration), cc.removeSelf()));
     acg.scene.addChild(lbl);
+    acg.ext._233s.push(lbl);
 };
--- test/danmaku.js
+++ test/danmaku.js
@@ -2,12 +2,21 @@ var acg = acg || {};
 acg.ext = acg.ext || {};
 
 //////// DANMAKU PART ////////
+acg.ext._233_add(0.1, 'xxx', cc.color.WHITE, js233.CMT_SLIDING);
+acg.ext._233_add(0.4, 'yyy', cc.color.RED, js233.CMT_SLIDING);
+acg.ext._233_add(0.2, 'cccc', cc.color.WHITE, js233.CMT_TOP_STICKY);
+acg.ext._233_add(1, 'zzzz', cc.color.WHITE, js233.CMT_TOP_STICKY);
+acg.ext._233_add(1.6, 'tttr', cc.color.YELLOW, js233.CMT_BOTTOM_STICKY);
+acg.ext._233_add(3, 'ffff', cc.color.YELLOW, js233.CMT_SLIDING);
+acg.ext._233_add(3.8, 'aaa', cc.color.GREEN, js233.CMT_SLIDING);
+acg.ext._233_add(5, 'QAQ', cc.color.GRAY, js233.CMT_SLIDING);
 //////////////////////////////
 
 var initStage = function () {
     acg.put(0, acg.img('h.png', {scale: 0.4}, ['delay', 4]));
     acg.commit();
     acg.ext.cp_enable();
+    acg.ext._233_commit();
     acg.travel(0);
 };
 
