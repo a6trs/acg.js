969f1bc542b8ba3994268f6e036baabc42e3be4d
Pisces000221 <1786762946@qq.com>
2015-03-22 17:44:44 +0800

    Improve compatibility (only Firefox supports string.endsWith)

--- acg/extensions/acg-res.js
+++ acg/extensions/acg-res.js
@@ -4,6 +4,6 @@ acg.ext = acg.ext || {};
 
 acg.ext._res_path = '.';
 acg.ext.set_res_path = function (path) {
-    if (path.endsWith('/')) path = path.substr(0, path.length - 1);
+    if (path.charAt(path.length - 1) === '/') path = path.substr(0, path.length - 1);
     acg.ext._res_path = path;
 };
